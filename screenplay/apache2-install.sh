f453a7e96f351e556f4e4641a4f7425b7da330ff
Davi Crystal <davicrystal@gmail.com>
2014-02-04 18:53:45 -0200

    Manual apache2 booting deleted

--- screenplay/apache2-install.sh
+++ screenplay/apache2-install.sh
@@ -8,11 +8,6 @@ echo "####   1/6 - STARTING THE APACHE SERVER INSTALATION    ####"
 echo "###########################################################"
 
 sudo apt-get install -y apache2 apache2-threaded-dev libapache2-mod-php5
-sudo touch /etc/init.d/startapache2
-sudo chmod 777 /etc/init.d/startapache2
-sudo echo '#! /bin/sh' > /etc/init.d/startapache2
-sudo echo "sudo /etc/init.d/apache2 start" >> /etc/init.d/startapache2
-sudo update-rc.d startapache2 defaults
 
 echo "###########################################################"
 echo "####      2/6 - SETTING THE VANGRANT SYMBOLIC LINKS    ####"
