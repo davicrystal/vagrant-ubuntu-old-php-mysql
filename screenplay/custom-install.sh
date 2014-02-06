d34c6f2cf8217681a78eed8167061d805fb30320
Davi Crystal <davicrystal@gmail.com>
2014-02-06 11:20:52 -0200

    Inclusão de rotinas para resolução de DNS e instalação do composer

--- screenplay/custom-install.sh
+++ screenplay/custom-install.sh
@@ -9,3 +9,14 @@ sudo a2enmod rewrite
 sudo sed -i '/AllowOverride None/c AllowOverride All' /etc/apache2/sites-available/default
 sudo service apache2 restart
 
+echo "CONFIGURANDO A RESOLUÇÃO DE DNS"
+
+sudo echo "nameserver 8.8.8.8" > /etc/resolvconf/resolv.conf.d/base
+sudo echo "nameserver 8.8.4.4" > /etc/resolvconf/resolv.conf.d/base
+sudo resolvconf -u
+
+echo "INSTALANDO COMPOSER"
+
+cd
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
