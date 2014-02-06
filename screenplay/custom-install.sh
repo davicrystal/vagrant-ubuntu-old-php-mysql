41cc02562b6607fd5d1b0d19792ebff96eef0ab7
Davi Crystal <davicrystal@gmail.com>
2014-02-06 10:36:55 -0200

    Ajustes para ambiente ecommerce

--- screenplay/custom-install.sh
+++ screenplay/custom-install.sh
@@ -3,5 +3,9 @@
 # PUT IN THIS FILE YOUR OWN CUSTOMIZED PROCEDURES
 # AUTHOR: YOU!
 
-echo "THERE IS NO CUSTOMIZED PROCEDURES!"
+echo "ATIVANDO REWRITE DO APACHE PARA URL AMIGÁVEL"
+
+sudo a2enmod rewrite
+sudo sed -i '/AllowOverride None/c AllowOverride All' /etc/apache2/sites-available/default
+sudo service apache2 restart
 
